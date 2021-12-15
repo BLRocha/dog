eea294fe3810f60c82102182671d00552a00570f
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-12-15 18:04:00 -0300

    Login page handles

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginLost from './LoginLost';
+import LoginReset from './LoginReset';
 
 const Login = () => {
-    return (<div></div>)
+    return (<div>
+        <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="criar" element={<LoginCreate />} />
+            <Route path="perdeu" element={<LoginLost />} />
+            <Route path="resete" element={<LoginReset />} />
+        </Routes>
+    </div>)
 }
 
 export default Login;
