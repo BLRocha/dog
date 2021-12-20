ebfa13a67aff9b9339923367ef5ae8ded61698f3
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-12-20 12:55:32 -0300

    Add check if the user is logged in

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginLost from './LoginLost';
 import LoginReset from './LoginReset';
+import { UserContext } from "../../Contexts/UserContext";
 
 const Login = () => {
+    const { login } = useContext(UserContext);
+
+    if (!login) <Navigate to="/conta"/>
     return (<div>
         <Routes>
             <Route path="/" element={<LoginForm />} />
