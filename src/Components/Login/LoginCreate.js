cb1a6820d92312de073d10da6267802d0dd768f0
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2022-01-05 19:32:13 -0300

    alter component for using custom hook useFetch and add erro message from api

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import Error from '../Helps/Error';
+
+import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../api';
+import { UserContext } from '../../Contexts/UserContext';
+import { useFetch } from '../../Hooks/useFetch';
+
 
 const LoginCreate = () => {
-    return (<div>
-        Login Criar
-        
-    </div>)
+  const { userLogin } = React.useContext(UserContext);
+  const  {loading, error, request} = useFetch();
+  
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+
+    const { url, options } = USER_POST({username: username.value, email: email.value, password: password.value})
+    const { response } = await request(url, options);
+    if (response.ok) {
+      await userLogin(username.value, password.value);
+    }
+  }
+
+  return (<section className='animeLeft'>
+    <h1 className='title'>Cadastre-se</h1>
+    <form onSubmit={handleSubmit} >
+      <Input id='ususario' type='text' label='Usuario' {...username}/>
+      <Input id='email' type='email' label='Email' {...email}/>
+      <Input id='password' type='password' label='Senha' {...password}/>
+      { loading ? <Button disabled>Cadastrondo...</Button>:<Button >Cadastro</Button>}
+      <Error error={error}/>
+    </form>
+  </section>)
 }
 
 export default LoginCreate;
